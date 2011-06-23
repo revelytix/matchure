f3077d374555b43e5fcc66c6a084e19d655382d9
Ryan Senior <rsenior@revelytix.com>
2011-06-23 10:31:02 -0500

    changed the version number to indicate a revelytix build

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject matchure "0.10.1"
+(defproject matchure "0.10.0-revelytix"
   :description "Idiomatic and powerful pattern matching in clojure"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]]
